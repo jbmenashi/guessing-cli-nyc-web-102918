20e50ce24e77e1fa982de3c3e7e7abc456590d93
Jacob Menashi <jbmenashi@gmail.com>
2018-10-15 23:30:22 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   answer = ""
   while answer != "exit"
+    answer = gets.chomp
     if answer.to_i == number
       puts "You guessed the correct number!"
     elsif answer.to_i != number
